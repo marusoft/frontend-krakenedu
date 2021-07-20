8799b809f5efd89e5c10cb1062042502eefbce8b
marusoft <alimi.kmaruf@gmail.com>
2021-07-20 08:29:10 +0100

    implement header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import "./header.css"
+import React from "react";
+import "./header.css";
 
 const Header = () => {
   return (
     <div className="header">
-      hello header
+      <div className="header-wrapper">
+        <div className="header-left">
+          <div className="award">
+            <span className="las la-award"></span>
+            <span className="suggest">6 Suggestions</span>
+          </div>
+          <div className="sort">
+            <span>Sort By</span>
+            <span className="vote">Most Upvotes</span>
+            <span className="las la-angle-down"></span>
+          </div>
+        </div>
+        <div className="header-right">
+        <button className="btn-feed"><span className="las la-plus"></span>Add Feedback</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
